0b9633a567b7eef44021865b767f1c25f781b59c
Noah Fontes <noah.fontes@puppet.com>
2019-10-31 15:52:57 -0700

    Add workflow test fixtures from existing workflow files found in the wild

--- pkg/workflow/v1/validate_test.go
+++ pkg/workflow/v1/validate_test.go
@@ -2,32 +2,29 @@ package v1_test
 
 import (
 	"io/ioutil"
+	"path/filepath"
+	"strings"
 	"testing"
 
 	v1 "github.com/puppetlabs/nebula-sdk/pkg/workflow/v1"
 	"github.com/stretchr/testify/require"
 )
 
-func TestValidYaml(t *testing.T) {
-	b, readErr := ioutil.ReadFile("testdata/valid.yaml")
-	require.NoError(t, readErr)
+func TestFixtureValidation(t *testing.T) {
+	fs, err := filepath.Glob("testdata/*.yaml")
+	require.NoError(t, err)
 
-	validationErr := v1.Validate(string(b))
-	require.NoError(t, validationErr)
-}
+	for _, file := range fs {
+		t.Run(filepath.Base(file), func(t *testing.T) {
+			b, err := ioutil.ReadFile(file)
+			require.NoError(t, err)
 
-func TestBadSyntax(t *testing.T) {
-	b, readErr := ioutil.ReadFile("testdata/badSyntax.yaml")
-	require.NoError(t, readErr)
-
-	validationErr := v1.Validate(string(b))
-	require.Error(t, validationErr)
-}
-
-func TestInvalidSchema(t *testing.T) {
-	b, readErr := ioutil.ReadFile("testdata/invalid.yaml")
-	require.NoError(t, readErr)
-
-	validationErr := v1.Validate(string(b))
-	require.Error(t, validationErr)
+			err = v1.Validate(string(b))
+			if strings.HasSuffix(file[:len(file)-len(filepath.Ext(file))], "_invalid") {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+			}
+		})
+	}
 }
