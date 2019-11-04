e706a5ea8efda2b9cc2335e4d868b395cce9e3aa
Noah Muldavin <nmuldavin@gmail.com>
2019-11-04 10:08:13 -0800

    Address PR feedback
    * Add test examples for approval and explicit container type steps
    * Small godoc fix

--- pkg/workflow/types/v1/validate.go
+++ pkg/workflow/types/v1/validate.go
@@ -2,7 +2,7 @@ package v1
 
 import "github.com/puppetlabs/nebula-sdk/pkg/util/typeutil"
 
-// Validates a yaml document according to the schema specification
+// ValidateYAML validates a yaml document according to the schema specification
 func ValidateYAML(y string) error {
 
 	return typeutil.ValidateYAMLString(WorkflowSchema, y)
