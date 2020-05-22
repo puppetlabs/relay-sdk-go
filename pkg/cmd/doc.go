812dc983decf1c17030e1da5aa450982c6dd5bef
Eric Sorenson <eric@puppet.com>
2020-05-21 21:34:18 -0700

    Fix documentation to build single-page docs
    This both brings it in line with what the website expects,
    it makes a more useful doc command than the previous 22-plus
    individual markdown pages.
    
    Also updates the 'long' description of the top-level command.

--- pkg/cmd/doc.go
+++ pkg/cmd/doc.go
@@ -1,10 +1,11 @@
+// go:generate go run generate_docs.go
+
 package cmd
 
 import (
-	"os"
+	"bytes"
 
 	"github.com/spf13/cobra"
-	"github.com/spf13/cobra/doc"
 )
 
 // NewDocCommand adds the 'doc' subcommand under the root 'ni'
@@ -12,38 +13,80 @@ func NewDocCommand() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "doc",
 		Short: "build command documentation",
-		Args:  cobra.MinimumNArgs(1),
-	}
-
-	cmd.AddCommand(newGenerateCommand())
-
-	return cmd
-}
-
-func newGenerateCommand() *cobra.Command {
-	cmd := &cobra.Command{
-		Use:   "generate",
-		Short: "Generate documentation",
 		Args:  cobra.NoArgs,
 		RunE:  genDocs,
 	}
-
-	cmd.Flags().StringP("target", "t", "./docs/", "target directory for output")
 
 	return cmd
 }
 
 func genDocs(cmd *cobra.Command, args []string) error {
-	targetDir, nil := cmd.Flags().GetString("target")
+	buf := new(bytes.Buffer)
+	rootcmd, err := NewRootCommand()
 
-	if err := os.MkdirAll(targetDir, 0755); err != nil {
-		cmd.Printf(`Failed to create target directory %s: %s`, targetDir, err.Error())
+	if err != nil {
 		return err
 	}
 
-	rootcmd, nil := NewRootCommand()
+	rootcmd.InitDefaultHelpCmd()
+	rootcmd.InitDefaultHelpFlag()
 
-	doc.GenMarkdownTree(rootcmd, targetDir)
+	name := rootcmd.CommandPath()
+
+	short := rootcmd.Short
+	long := rootcmd.Long
+
+	buf.WriteString("## " + name + "\n\n" + short + "\n\n")
+	buf.WriteString("### Synopsis\n\n" + long + "\n\n")
+	buf.WriteString("### Subcommand Usage\n\n")
+
+	children := rootcmd.Commands()
+
+	if err := genChildMarkdown(children, buf); err != nil {
+		return err
+	}
+
+	flags := rootcmd.PersistentFlags()
+	if flags.HasAvailableFlags() {
+		buf.WriteString("### Global flags\n```\n")
+		buf.WriteString(flags.FlagUsages() + "\n```\n")
+	}
+	rootcmd.OutOrStdout().Write(buf.Bytes())
+
+	return nil
+
+}
+
+// For brevity, we only want to generate docs for 'leaf' commands, i.e.
+// only "ni output get", not "ni output"
+func genChildMarkdown(children []*cobra.Command, buf *bytes.Buffer) error {
+
+	for _, child := range children {
+		if !child.IsAvailableCommand() || child.IsAdditionalHelpTopicCommand() {
+			continue
+		}
+		if child.Runnable() {
+			usage := child.UseLine()
+			buf.WriteString("**`" + usage + "`** -- " + child.Short + "\n")
+			long := child.Long
+			if len(long) > 0 {
+				buf.WriteString("  " + child.Long + "\n")
+			}
+			flags := child.NonInheritedFlags()
+			if flags.HasAvailableFlags() {
+				buf.WriteString("```\n")
+				flags.SetOutput(buf)
+				flags.PrintDefaults()
+				buf.WriteString("```\n")
+			}
+			buf.WriteString("\n")
+		}
+		// Because commands can be nested arbitrarily deep, this recurses into
+		// the current command's children and generates their docs too
+		grandchildren := child.Commands()
+		genChildMarkdown(grandchildren, buf)
+
+	}
 
 	return nil
 
